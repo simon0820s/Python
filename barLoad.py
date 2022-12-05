4a8c3091fc400217144de0d19c3cccb0c198e1c9
simon0820s <simon0820s@gmail.com>
2022-12-05 10:26:50 -0500

    barLoad

---
+++ barLoad.py
@@ -0,0 +1,16 @@
+
+
+import time
+
+
+limit = 40
+def ProgressBar(segment, total, length):
+    porcent = segment / total
+    completed = int (porcent * length)
+    rest = length - completed
+    bar = f"[{'#' * completed}{'^'*rest}{porcent: .2%}]"
+    return bar
+
+for i in range(limit+1):
+    time.sleep(0.08)
+    print(ProgressBar(i,limit,200),end = "\r")
