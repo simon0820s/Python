6f057f3a3d06d973133273ca1c19027dcc8edac7
simon0820s <simon0820s@gmail.com>
2022-12-07 19:35:24 -0500

    list comperhentions 2

---
+++ list_comperhentions.py
@@ -0,0 +1,11 @@
+def main():
+    #lista generada con comperhentions
+    numbers=[element for element in range(1,11)]
+    print(numbers)
+    print('')
+
+    #lista con elementos hasta el numero 10.000 multiplos de 4,6,9
+    list=[i for i in range(10000) if i%4==0 and i%6==0 and i%9==0]
+    print(list)
+if __name__=='__main__':
+    main()
--- list_comprehentions.py
+++
@@ -1,6 +0,0 @@
-def main():
-    #lista con elementos hasta 5 digitos, multiplos de 4,6,9
-    list=[i for i in range(10000) if i%4==0 and i%6==0 and i%9==0]
-    print(list)
-if __name__=='__main__':
-    main()
