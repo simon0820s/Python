44331e18d23700891fb41c667c96eefaa539e90d
simon0820s <simon0820s@gmail.com>
2022-12-08 10:35:03 -0500

    double iteration with map

--- map.py
+++ map.py
@@ -9,3 +9,12 @@ print("Normal modify: "+str(v2))
 #Map modify
 v2map=list(map((lambda i:i*2),v1))
 print("Map modify: "+str(v2map))
+
+#Double iteration
+n1=(1,2,3,4)
+n2=(5,6,7)
+print("double iteration")
+print('n1 = '+str(n1))
+print('n2 = '+str(n2))
+result=list(map(lambda x,y: x+y,n1,n2))
+print('result = '+str(result))
