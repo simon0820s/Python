c75d99037df35eae445100cb5344731ef9d1d933
simon0820s <simon0820s@gmail.com>
2022-12-09 10:12:52 -0500

    iterador, iterando manualmente un iterador

---
+++ iter.py
@@ -0,0 +1,18 @@
+#Iterador
+my_iter=iter(range(1,11))
+
+#Iterador automatico
+""" for i in my_iter:
+    print(i) """
+
+#Iterando manualmente
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
