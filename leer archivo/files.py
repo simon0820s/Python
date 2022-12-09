be1f82fd17e447550a5265e5643a3c17508c7d34
simon0820s <simon0820s@gmail.com>
2022-12-09 11:24:41 -0500

    leer y modificar archivo

---
+++ leer archivo/files.py
@@ -0,0 +1,26 @@
+file = open('./leer archivo/text.txt','r')
+#Lectura automatica
+#print(file.read())
+#Lectura manual
+""" print(file.readline())
+print(file.readline())
+print(file.readline()) """
+
+#Cerrar archivo
+#file.close()
+
+#Para leer un archivo muy grande completo es mejor usar for
+""" for line in file:
+    print(line)
+file.close() """
+
+#Esta sentencia cierra el archivo automaticamente al finalizar el bloque
+#Escribir en archivo
+with open('./leer archivo/text.txt','w+') as file:
+    for i in range(10,21):
+        for line in file:
+            print(line)
+        file.write("\n")
+        file.write(str(i))
+    
+
