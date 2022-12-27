f18630c571f2ac5ac1e31c6ce5b40cea0f8fc1f5
simon0820s <simon0820s@gmail.com>
2022-12-26 20:39:34 -0500

    what that forms

--- flask/main.py
+++ flask/main.py
@@ -1,10 +1,21 @@
 from flask import Flask,make_response,redirect,request,render_template,session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired
 app=Flask(__name__)
 bootstrap=Bootstrap(app)
 app.config['SECRET_KEY']='SUPER SECRETO'
 
 todos=['Comer','Dormir','Programar']
+
+class LoginForm(FlaskForm):
+    name=StringField('User name',validators=[DataRequired()])
+    password=PasswordField('Password',validators=[DataRequired()])
+    submit=SubmitField('Send')
+
+
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html',error=error)
@@ -17,14 +28,17 @@ def didnt_completed(error):
 @app.route('/')
 def index():
     user_ip=request.remote_addr
-    response=make_response(redirect('/welcome'))
+    response=make_response(redirect('/hello'))
     session['user_ip']=user_ip
     return response
-@app.route('/welcome')
-def welcome():
-    user_ip=session.get['user_ip']
+
+@app.route('/hello')
+def hello():
+    user_ip=request.remote_addr
+    login_form=LoginForm()
     context={
         'user_ip':user_ip,
-        'todos':todos
+        'todos':todos,
+        'login_form':login_form
     }
     return render_template('hello.html',**context)
