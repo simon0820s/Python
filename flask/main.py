525f8ffc24ae74db1055439c758eb67363dd050d
simon0820s <simon0820s@gmail.com>
2022-12-28 10:57:38 -0500

    using flask testing

--- flask/main.py
+++ flask/main.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired
+import unittest
 app=Flask(__name__)
 bootstrap=Bootstrap(app)
 app.config['SECRET_KEY']='SUPER SECRETO'
@@ -13,6 +14,11 @@ class LoginForm(FlaskForm):
     name=StringField('User name',validators=[DataRequired()])
     password=PasswordField('Password',validators=[DataRequired()])
     submit=SubmitField('Send')
+
+@app.cli.command()
+def test():
+    tests=unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
 
 
 
