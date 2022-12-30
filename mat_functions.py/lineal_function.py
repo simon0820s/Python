dcb8a33dc4f1fe7b4e07cdd0544db58ac7259e02
simon0820s <simon0820s@gmail.com>
2022-12-30 15:04:01 -0500

    lineal functions

--- mat_functions.py/lineal_function.py
+++ mat_functions.py/lineal_function.py
@@ -1,5 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
 n=100
+m=10
+b=5
 def f(x):
     return m*x+b
 
-x=
+x=np.linspace(-10,10,num=n)
+
+y=f(x)
+
+fig,ax=plt.subplots()
+ax.plot(x,y)
+ax.grid()
+plt.show()
