8ffe7806852a99dc77e84cacf920d8fc4881a64e
simon0820s <simon0820s@gmail.com>
2023-02-03 22:05:21 -0500

    flask

--- flask/main.py
+++ flask/main.py
@@ -1,60 +1,20 @@
-from flask import Flask, flash,make_response,redirect,request,render_template,session,url_for
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField,PasswordField,SubmitField
-from wtforms.validators import DataRequired
-import unittest
+from flask import Flask,request,make_response,redirect,render_template
+
 app=Flask(__name__)
-bootstrap=Bootstrap(app)
-app.config['SECRET_KEY']='SUPER SECRETO'
 
-todos=['Comer','Dormir','Programar']
-
-class LoginForm(FlaskForm):
-    name=StringField('User name',validators=[DataRequired()])
-    password=PasswordField('Password',validators=[DataRequired()])
-    submit=SubmitField('Send')
-
-@app.cli.command()
-def test():
-    tests=unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner().run(tests)
-
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html',error=error)
-
-@app.errorhandler(500)
-def didnt_completed(error):
-    return render_template('500.html',error=error)
-
-
-@app.route('/')
+@app.route("/")
 def index():
     user_ip=request.remote_addr
-    response=make_response(redirect('/hello'))
-    session['user_ip']=user_ip
+    response=make_response(redirect("/hello"))
+    response.set_cookie("user_ip",user_ip)
+
     return response
 
-@app.route('/hello',methods=['GET','POST'])
-def hello():
-    user_ip=session.get('user_ip')
-    login_form=LoginForm()
-    name=session.get('name')
-    context={
-        'user_ip':user_ip,
-        'todos':todos,
-        'login_form':login_form,
-        'name':name
-    }
-    
-    if login_form.validate_on_submit():
-        name=login_form.name.data
-        session['name']=name
-        flash('User name registered!!')
-        return redirect(url_for('index'))
+@app.route("/hello")
+def hello_world():
+    user_ip=request.cookies.get("user_ip")
+    return render_template("hello.html",user_ip=user_ip)
 
 
-    return render_template('hello.html',**context)
+if __name__=='__main__':
+    app.run()
